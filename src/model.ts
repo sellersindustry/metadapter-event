f4d692332748d4af85441b5f620c0ae575f5aa06
Evan Sellers <github@sellersew.com>
2024-03-27 09:38:06 -0400

    Default Generic for Init Properties

--- src/model.ts
+++ src/model.ts
@@ -29,7 +29,7 @@ export type ConfigByType = {
     [Type.Skeleton]: SkeletonConfig
 };
 
-export type InitProperties<T extends Type> = {
+export type InitProperties<T extends Type=undefined> = {
     type: T;
     config: ConfigByType[T];
 };
